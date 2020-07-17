e14417ef581a973d38a0eedd0f97c3aa6f29d779
Ellie <e3e@disroot.org>
2020-07-17 20:57:58 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="augaudio",
+    version="1.0.0",
+    author="Ellie",
+    author_email="e3e@disroot.org",
+    description="A simple audio data augmentation package",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="ptrpaws",
+    project_urls={
+        "Bug Tracker": "https://github.com/ptrpaws/augaudio/issues",
+        "Source Code": "https://github.com/ptrpaws/augaudio",
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+          'numpy >= 1.16.2',
+          'librosa >= 0.7.2',
+          ],
+    license="Apache",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+		"Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering",
+    ],
+)
